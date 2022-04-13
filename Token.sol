b1f28c8198d27c45f06cbc7526e4b9cdc232b841
Deborah Emeni <debemenitammy>
2022-04-13 20:57:33 +0100

    ghvh

---
+++ Token.sol
@@ -0,0 +1,127 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.4.22 <0.9.0;
+
+interface IERC20 {
+
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address account) external view returns (uint256);
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    function transfer(address recipient, uint256 amount) external returns (bool);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+contract ErcToken is IERC20 {
+    using SafeMath for uint256;
+
+    string private _name;
+    string private _symbol;
+    uint8 private _decimals;
+
+    mapping (address => uint256) private _balances;
+    mapping (address => mapping (address => uint256)) private _allowances;
+
+    uint256 private _totalSupply;
+
+    constructor (string memory token_name, string memory token_symbol, int memory total) public {
+    _name = token_name;
+    _symbol = token_symbol;
+    _decimals = 18;
+    _totalSupply = total;
+    _balances[msg.sender] = _totalSupply;
+  }
+
+  function name() public view returns (string memory) {
+    return _name;
+  }
+
+  function symbol() public view returns (string memory) {
+    return _symbol;
+  }
+
+  function decimals() public view returns (uint8) {
+    return _decimals;
+  }
+
+  function totalSupply() public view override returns (uint256) {
+    return _totalSupply;
+  }
+
+  function balanceOf(address account) public view override returns (uint256) {
+    return _balances[account];
+  }
+
+  function transfer(address recipient, uint256 amount) public virtual override returns (bool) {
+    _transfer(_msgSender(), recipient, amount);
+    return true;
+  }
+
+  function allowance(address owner, address spender) public view virtual override returns (uint256) {
+    return _allowances[owner][spender];
+  }
+
+  function approve(address spender, uint256 amount) public virtual override returns (bool) {
+    _approve(_msgSender(), spender, amount);
+    return true;
+  }
+
+   function transferFrom(address sender, address recipient, uint256 amount) public virtual override returns (bool) {
+      _transfer(sender, recipient, amount);
+      _approve(sender, _msgSender(), _allowances[sender][_msgSender()].sub(amount,
+      "ERC20: transfer amount exceeds allowance"));
+      return true;
+    }
+
+    function _transfer(
+          address from,
+          address to,
+          uint256 amount
+      ) internal virtual {
+          require(from != address(0), "ERC20: transfer from the zero address");
+          require(to != address(0), "ERC20: transfer to the zero address");
+
+          _beforeTokenTransfer(from, to, amount);
+
+          uint256 fromBalance = _balances[from];
+          require(fromBalance >= amount, "ERC20: transfer amount exceeds balance");
+          unchecked {
+              _balances[from] = fromBalance - amount;
+          }
+          _balances[to] += amount;
+
+          emit Transfer(from, to, amount);
+
+          _afterTokenTransfer(from, to, amount);
+    }
+
+      function _approve(
+        address owner,
+        address spender,
+        uint256 amount
+    ) internal virtual {
+        require(owner != address(0), "ERC20: approve from the zero address");
+        require(spender != address(0), "ERC20: approve to the zero address");
+
+        _allowances[owner][spender] = amount;
+        emit Approval(owner, spender, amount);
+    }
+
+}
+
+library SafeMath {
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+      assert(b <= a);
+      return a - b;
+    }
+
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+      uint256 c = a + b;
+      assert(c >= a);
+      return c;
+    }
+}
